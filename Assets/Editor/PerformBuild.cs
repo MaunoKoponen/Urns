c448bb165b5e6d5669d1b348f0b5c63fbc134025
Mauno Koponen <maunokoponen@gmail.com>
2018-09-11 10:19:52 +0300

    another commit

---
+++ Assets/Editor/PerformBuild.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+class PerformBuild
+{
+    static void Build()
+    {
+        string[] scenes = { "Assets/Scenes/SampleScene.unity" };
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = "AndroidTestBuild",
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+		};
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+		
+    }
+}
